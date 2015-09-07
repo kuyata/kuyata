0b6d06a6c8486e0f9e1b5de7fb16c2b68f017eb1
Russell Paterson <russell-h-p@outlook.com>
2015-09-07 16:39:16 +0200

    Recovery categories & items data

---
+++ app/components/common/data/categories.js
@@ -0,0 +1,90 @@
+var categoriesData = [
+	  {
+		"name": "Things",
+		"slug": "things",
+		"subcat1":
+		[
+		  {
+			"name": "Computer Components",
+			"slug": "computercomponents",
+			"subcat2":
+			[
+			  {"name":"Laptop", "slug":"laptop"},
+			  {"name":"Internet", "slug":"internet"}
+			]
+		  },
+		  {
+			"name": "Buy/Sell",
+			"slug": "buysell",
+			"subcat2":
+			[
+			  {"name":"TV", "slug":"tv"},
+			  {"name":"Currency", "slug":"currency"}
+			]
+		  },
+		  {
+			"name": "Services",
+			"slug": "services",
+			"subcat2":
+			[
+			  {"name":"Photo/Video", "slug":"photovideo"},
+			  {"name":"Others", "slug":"others"}
+			]
+		  },
+		  {
+			"name": "Cars",
+			"slug": "cars",
+			"subcat2":
+			[
+			  {"name":"Cars", "slug":"cars"},
+			  {"name":"Motorbikes", "slug":"motorbikes"}
+			]
+		  },
+		  {
+			"name": "Home",
+			"slug": "home",
+			"subcat2":
+			[
+			  {"name":"Exchange", "slug":"exchange"},
+			  {"name":"Rent", "slug":"rent"}
+			]
+		  },
+		  {
+			"name": "Job",
+			"slug": "job",
+			"subcat2":
+			[
+			  {"name":"Job offer", "slug":"joboffer"},
+			  {"name":"Looking for a job", "slug":"lookingforjob"}
+			]
+		  }
+		]
+	  },
+	  {
+		"name": "Eatup",
+		"slug": "eatup",
+		"subcat1":
+		[
+		  {
+			"name": "Starter",
+			"slug": "starter",
+			"subcat2":
+			[
+			  {"name":"Chips", "slug":"chips"},
+			  {"name":"Olives", "slug":"olives"}
+			]
+		  },
+		  {
+			"name": "Desserts",
+			"slug": "desserts",
+			"subcat2":
+			[
+			  {"name":"Cake", "slug":"Cake"},
+			  {"name":"Yogurt", "slug":"yogurt"}
+			]
+		  }
+		]
+	  }
+	];
+
+export {categoriesData};
