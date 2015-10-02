8a213ed39ab64196713a6e093d9edf46a8029c11
Stephen Stewart <sstewart_87@outlook.com>
2015-10-02 19:49:12 +0200

    Components. Delete old category and item components

--- app/components/categories/categories.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import CategoriesService from './categories-service'
-
-export default angular.module('app.models.categories', [
-
-])
-
-.service('categories', CategoriesService);
