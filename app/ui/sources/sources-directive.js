ad4c7e05fca5b701152269dc03bbf245115a447f
Russell Paterson <russell-h-p@outlook.com>
2015-10-05 11:04:32 +0200

    Refactoring UI structure and create initial 'source-directive'

---
+++ app/ui/sources/sources-directive.js
@@ -0,0 +1,48 @@
+/**
+ * @fileOverview
+ *
+ * This file contains the sources directive and controller
+ */
+
+import {sourcesData} from './../../components/common/data/sources';
+
+/**
+ * Sources directive factory
+ *
+ * @returns {object} DDO directive definition object
+ * @constructor
+ */
+export default function SourcesDirective(){
+	return {
+		scope: {},
+		controllerAs: 'vm',
+		controller: SourcesController,
+		bindToController: true,
+		templateUrl: 'ui/sources/sources.html'
+	};
+}
+
+
+/**
+ * Sources directive controller
+ *
+ * @constructor
+ * @ngInject
+ */
+class SourcesController {
+	constructor(SourceManager, CategoryManager) {
+
+		// Create sample initial data
+		SourceManager.createSampleData(sourcesData);
+
+		SourceManager.findList().then(() => {
+			this.sources = SourceManager.data.list;
+			console.log(SourceManager.data.list);
+		});
+
+		CategoryManager.findList().then(() => {
+			this.categories = CategoryManager.data.list;
+			console.log(CategoryManager.data.list);
+		});
+	}
+}
