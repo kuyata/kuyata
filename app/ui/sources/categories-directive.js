53565f90d8066ebf93f91df444f74ce684d9f6ae
Russell Paterson <russell-h-p@outlook.com>
2015-10-13 11:21:27 +0200

    ui. Categories. Apply activeItem and isOpen

--- app/ui/sources/categories-directive.js
+++ app/ui/sources/categories-directive.js
@@ -33,10 +33,17 @@ export default function CategoriesDirective(){
 class CategoriesController {
 	constructor(SourceManager, $state) {
 		this.state = $state;
+		this.SourceManager = SourceManager;
+
 		// Get category list filtered by source id
 		SourceManager.getCategoriesTreeBySourceId(this.sourceId).then((sourceTree) => {
 			this.categories = sourceTree;
 		});
+	}
+
+	activeItem(categoryId, ev) {
+		this.SourceManager.CategoryManager.setCurrentItemId(categoryId);
+		ev.stopPropagation();
 	}
 
 	itemListByCategory(source, category) {
