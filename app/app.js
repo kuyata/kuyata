8826bb697f1fb8beedbe1ac7b45d7eceb3541783
Russell Paterson <russell-h-p@outlook.com>
2015-10-19 19:22:19 +0200

    App. Set sql adapter. jsdata on debug mode & knex on debug mode

--- app/app.js
+++ app/app.js
@@ -16,11 +16,11 @@ db.close();
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import jsData from 'js-data';
-import DSLocalStorageAdapter from 'js-data-localstorage';
+import DSSqlAdapter from 'js-data-sql';
 import jsDataAngular from 'js-data-angular';
 import uiBootstrap from 'angular-bootstrap';
 
-import sources from './ui/sources/sources'
+import sources from './ui/sources/sources';
 
 export default angular.module('app', [
     uiRouter,
@@ -29,7 +29,7 @@ export default angular.module('app', [
     sources.name
 ])
 
-.config(($stateProvider, $urlRouterProvider) => {
+.config(($stateProvider, $urlRouterProvider, DSProvider) => {
 
     $stateProvider
 
@@ -38,11 +38,67 @@ export default angular.module('app', [
         });
 
     $urlRouterProvider.otherwise("/");
+    DSProvider.defaults.debug = true;
 
 })
 
 .run((DS) => {
 
-    DS.registerAdapter('localstorage', new DSLocalStorageAdapter(), { default: true });
+    var adapter = new DSSqlAdapter({
+        client: 'sqlite3', // or "pg" or "sqlite3"
+        connection: {
+            filename: "./database.sqlite"
+        },
+        debug: true
+    });
 
+
+    adapter.query.schema.hasTable('source').then(function (exists) {
+        if (!exists) {
+            return query.schema.createTable('source', function (t) {
+                t.string('id');
+                t.string('name');
+                t.string('src_id');
+                t.string('status');
+                t.string('url');
+                t.string('created_on');
+                t.string('updated_on');
+            });
+        }
+    });
+
+        adapter.query.schema.hasTable('source').then(function (exists) {
+            if (!exists) {
+                return query.schema.createTable('source', function (t) {
+                    t.string('id');
+                    t.string('name');
+                    t.string('source_id');
+                    t.string('parent_category_id');
+                    t.string('src_id');
+                    t.string('status');
+                    t.string('created_on');
+                    t.string('updated_on');
+                });
+            }
+        });
+
+        adapter.query.schema.hasTable('source').then(function (exists) {
+            if (!exists) {
+                return query.schema.createTable('source', function (t) {
+                    t.string('source_id');
+                    t.string('category_id');
+                    t.string('subcategory_id');
+                    t.string('title');
+                    t.string('body');
+                    t.string('author');
+                    t.string('url');
+                    t.string('status');
+                    t.string('src_date');
+                    t.string('created_on');
+                    t.string('updated_on');
+                });
+            }
+        });
+
+    DS.registerAdapter('sql', adapter, { default: true });
 });
