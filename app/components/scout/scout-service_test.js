a495aff6ac43cb8414ac70aac81ebb168c1bfaa8
Stephen Stewart <sstewart_87@outlook.com>
2015-10-21 16:11:42 +0200

    ScoutService. tests. commented parsetest

--- app/components/scout/scout-service_test.js
+++ app/components/scout/scout-service_test.js
@@ -8,7 +8,11 @@ import angular from 'angular';
 import ngMocks from 'angular-mocks';
 
 import scoutModule from './scout';
+
 import resolveUrl from "resolve-url";
+import iconv from 'iconv-lite';
+import FeedParser from 'feedparser';
+import Stream from 'stream';
 
 describe("Scout", () => {
     let rootScope, q, http, httpBackend, Scout;
@@ -31,6 +35,36 @@ describe("Scout", () => {
         + '</item>'
         + '</channel>'
         + '</rss>';
+
+
+    let atomXml = '<?xml version="1.0" encoding="utf-8"?>'
+        + '<feed xmlns="http://www.w3.org/2005/Atom">'
+        + '<title>Example Feed</title>'
+        + '<subtitle>A subtitle.</subtitle>'
+        + '<link href="http://example.org/feed/" rel="self" />'
+        + '<link href="http://example.org/" />'
+        + '<id>urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6</id>'
+        + '<updated>2003-12-13T18:30:02Z</updated>'
+        + '<entry>'
+        + '<title>Atom-Powered Robots Run Amok</title>'
+        + '<link href="http://example.org/2003/12/13/atom03" />'
+        + '<link rel="alternate" type="text/html" href="http://example.org/2003/12/13/atom03.html"/>'
+        + '<link rel="edit" href="http://example.org/2003/12/13/atom03/edit"/>'
+        + '<id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>'
+        + '<updated>2003-12-13T18:30:02Z</updated>'
+        + '<summary>Some text.</summary>'
+        + '<content type="xhtml">'
+        + '<div xmlns="http://www.w3.org/1999/xhtml">'
+        + '<p>This is the entry content.</p>'
+        + '</div>'
+        + '</content>'
+        + '<author>'
+        + '<name>John Doe</name>'
+        + '<email>johndoe@example.com</email>'
+        + '</author>'
+        + '</entry>'
+        + '</feed>';
+
 
     let htmlLinkAtom = '<html><head><link href="http://atom-xml" type="application/atom+xml"></head><body></body></html>';
     let htmlLinkRss = '<html><head><link href="http://rss2-xml" type="application/rss+xml"></head><body></body></html>';
@@ -103,6 +137,22 @@ describe("Scout", () => {
             });
             httpBackend.flush();
         });
+    });
 
+    xdescribe("parse()", () => {
+
+        beforeEach(done => _setup(done));
+
+        it("should parse RSSv2 feed", () => {
+            Scout.parse(rss2Xml).then((result) => {
+                expect(result.meta.title).toBe('RSS Title');
+                expect(result.meta.link).toBe('http://www.example.com/main.htmls');
+                expect(result.articles.length).toBe(1);
+                expect(result.articles[0].title).toBe('Example entry');
+
+            }).catch((e) => {
+                console.log(e);
+            });
+        });
     });
 });
