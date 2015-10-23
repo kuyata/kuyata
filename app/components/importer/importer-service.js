7f2d2c252ad54f27f3dc22537cedda00a47824e6
Stephen Stewart <sstewart_87@outlook.com>
2015-10-23 17:55:17 +0200

    Importer. Initial import method

--- app/components/importer/importer-service.js
+++ app/components/importer/importer-service.js
@@ -1,11 +1,36 @@
 export default class Importer {
 
     constructor($q, SourceManager, CategoryManager, ItemManager){
-
-        this.url = null;
         this.$q = $q;
         this.SourceManager = SourceManager;
         this.CategoryManager = CategoryManager;
         this.ItemManager = ItemManager;
+    }
+
+    /**
+     * Method to explore ddbb and analize if follow a create or update flow
+     * from a normalized feed Object
+     *
+     * @param url
+     */
+    import(feed) {
+
+        //TODO: manage create and update flows
+        //if new source
+        if(!this.SourceManager.exists(feed.source)){
+
+            let itemIds = {};
+
+            this.SourceManager.createSource(feed.source).then((sourceId) => {
+                // separate feed.categories ?? or all on a tree struct
+                // if source already exists, add only new items (check date??)
+                // if items are new
+                itemIds.source_id = sourceId;
+                this.ItemManager.createItems(feed.items, itemIds);
+            }).catch((e) => {console.log(e);});
+
+        }
+
+
     }
 }
