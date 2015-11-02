23a48dd8a68fc19844027e909f0719500a165b46
Russell Paterson <russell-h-p@outlook.com>
2015-11-02 17:40:56 +0100

    Exporter. implement streams to get sources and items from db

--- app/components/exporter/exporter-service.js
+++ app/components/exporter/exporter-service.js
@@ -1,6 +1,47 @@
+/**
+ * Exporter is a middleware between Core and Exporters.
+ * Read data from Core and and send to specific-exporters who request it through specific-exporters-ui
+ *
+ * @class
+ */
 export default class Exporter {
 
-    constructor($q){
+    constructor($q, $rootScope, DS, SourceManager, CategoryManager, ItemManager){
         this.$q = $q;
+        this.SourceManager = SourceManager;
+        this.CategoryManager = CategoryManager;
+        this.ItemManager = ItemManager;
+        this.adapter;
+        this.redableStream;
+
+        $rootScope.$on("adapter:ready", () => {
+            this.adapter = DS.adapters.sql;
+        });
+
     }
+
+    /**
+     * Get a expose readable stream from adapter with requested sources data
+     *
+     * @param sourceIds, an array of source db ids requested
+     * @returns an stream from adapter
+     */
+    getSourcesStream(sourceIds) {
+        let readableStream = this.adapter.query.select('name', 'guid', 'url', 'last_feed_date', 'type')
+            .from('source').whereIn('id', sourceIds).stream();
+        return readableStream;
+    }
+
+    /**
+     * Get a expose readable stream from adapter with requested items data
+     *
+     * @param sourceGuid, an array of source db guid requested
+     * @returns an stream from adapter
+     */
+    getItemsStream(sourceGuid) {
+        let readableStream = this.adapter.query.select('title', 'body', 'author', 'guid', 'url', 'last_feed_date')
+            .from('item').whereIn('orig_source_id', sourceGuid).stream();
+        return readableStream;
+    }
+
 }
--- app/components/exporter/exporter.js
+++ app/components/exporter/exporter.js
@@ -2,6 +2,14 @@ import angular from 'angular';
 
 import Exporter from './exporter-service.js';
 
-module.exports = angular.module('app.exporter', [])
+import SourceManager from '../source/source-manager/source-manager.js'
+import CategoryManager from '../category/category-manager/category-manager.js'
+import ItemManager from '../item/item-manager/item-manager.js'
+
+module.exports = angular.module('app.exporter', [
+    SourceManager.name,
+    CategoryManager.name,
+    ItemManager.name
+])
 
 .service('Exporter', Exporter);
