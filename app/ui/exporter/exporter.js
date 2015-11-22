3cfe032e0371363a025a2929d771796df1a81ed2
Russell Paterson <russell-h-p@outlook.com>
2015-11-22 18:04:22 +0100

    Comment old importers and exporters references

--- app/ui/exporter/exporter.js
+++ app/ui/exporter/exporter.js
@@ -4,11 +4,11 @@ import uiBootstrap from 'angular-bootstrap';
 import ExporterDirective from './exporter-directive';
 
 // ui exporter list
-import defaultExporter from './exporters/default-exporter/default-exporter';
+//import defaultExporter from './exporters/default-exporter/default-exporter';
 
 export default angular.module('exporter', [
-	'ui.bootstrap',
-	defaultExporter.name
+	'ui.bootstrap'
+	//defaultExporter.name
 ])
 
 .directive('exporter', ExporterDirective);
--- app/ui/importer/importer.js
+++ app/ui/importer/importer.js
@@ -4,15 +4,15 @@ import uiBootstrap from 'angular-bootstrap';
 import ImporterDirective from './importer-directive';
 
 // ui importer list
-import defaultImporter from './importers/default-importer/default-importer';
-import rssImporter from './importers/rss-importer/rss-importer';
-import packageImporter from './importers/package-importer/package-importer';
+//import defaultImporter from './importers/default-importer/default-importer';
+//import rssImporter from './importers/rss-importer/rss-importer';
+//import packageImporter from './importers/package-importer/package-importer';
 
 export default angular.module('importer', [
-	'ui.bootstrap',
-	defaultImporter.name,
-	rssImporter.name,
-	packageImporter.name
+	'ui.bootstrap'
+	//defaultImporter.name,
+	//rssImporter.name,
+	//packageImporter.name
 ])
 
 .directive('importer', ImporterDirective);
