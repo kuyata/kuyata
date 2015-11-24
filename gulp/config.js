1e45203cca2b0456f8af36c86ef7e5b2c1871f6d
Russell Paterson <russell-h-p@outlook.com>
2015-11-24 15:09:41 +0100

    gulp. Add plugins to browserify task to build them as independent compiled files

--- gulp/config.js
+++ gulp/config.js
@@ -27,11 +27,35 @@ module.exports = {
         extensions: [],
         // A separate bundle will be generated for each
         // bundle config in the list below
-        bundleConfigs: [{
-            entries: '../../app/app.js',
-            dest: dest + '/scripts/',
-            outputName: 'app.js'
-        }],
+        bundleConfigs: [
+            {
+                entries: '../../app/app.js',
+                dest: dest + '/scripts/',
+                outputName: 'app.js',
+                runtime: true
+            },
+            // Add here your plugin directive entry point, if you want compile your plugin
+            {
+                entries: '../../app/components/importers/default-importer/ui/default-importer.js',
+                dest: dest + '/scripts/importers/default-importer/',
+                outputName: 'default-importer.js'
+            },
+            {
+                entries: '../../app/components/importers/package-importer/ui/package-importer.js',
+                dest: dest + '/scripts/importers/package-importer/',
+                outputName: 'package-importer.js'
+            },
+            {
+                entries: '../../app/components/importers/rss-importer/ui/rss-importer.js',
+                dest: dest + '/scripts/importers/rss-importer/',
+                outputName: 'rss-importer.js'
+            },
+            {
+                entries: '../../app/components/exporters/default-exporter/ui/default-exporter.js',
+                dest: dest + '/scripts/exporters/default-exporter/',
+                outputName: 'default-exporter.js'
+            }
+        ],
         insertGlobalVars: {
             process: false
         }
