18ae5e29d29766d16a8f455bdd99b762436af59f
Russell Paterson <russell-h-p@outlook.com>
2015-12-04 21:16:38 +0100

    I/E. Add compiled translate files

---
+++ app/components/exporters/default-exporter/ui/translations/es.js
@@ -0,0 +1,5 @@
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('es', {"Export":"Exportar"});
+/* jshint +W100 */
+}]);
---
+++ app/components/importers/rss-importer/ui/translations/es.js
@@ -0,0 +1,5 @@
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('es', {"Type url and push ENTER":"Escribe una url y pulsa enter"});
+/* jshint +W100 */
+}]);
